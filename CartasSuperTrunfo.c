b178b1ee657b688e539d0cad4bce7abec4873967
rixarde89 <rixarde.4@gmail.com>
2025-07-30 16:11:31 +0000

    Montando estrutura

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,14 +3,30 @@
 
 int main() {
     printf("Desafio Super Trunfo!\n");
-    char estado;
-    char codigo[20];
-    char cidade[20];
+
+    char estado1, estado2;
+    char codigo1, codigo2[10];
+    char cidade1, cidade2[50];
     int populacao1, populacao2;
-    int pontosturisticos1, pontosturisticos1;
+    int pontosturisticos1, pontosturisticos2;
     float area1, area2;
     float pib1, pib2;
-    
+
+   printf("--- Cadastro da primera carta ---\n");
+
+   printf("Digite o estado da primera carta (apenas uma letra ):");
+   scanf(" %c", &estado1);
+   printf("A letra do Estado é: %c\n", estado1);
+   
+   printf("Digite o código da primeira carta (Ex:'A01'):");
+   scanf("%s", codigo1);
+   printf("Código da primera carta é: %s\n", codigo1);
+
+   printf("Digite o nome da primeira cidade");
+   scanf("%s", cidade1);
+   printf("A primeira cidade é:");
+
+  
     
 
 
