2dbef81464e2c3d1155dca8cabb54102560aa344
rixarde89 <rixarde.4@gmail.com>
2025-08-12 13:50:23 +0000

    Media aluno final

--- Mediaaluno/mediaaluno.c
+++ Mediaaluno/mediaaluno.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
 
 /* Criar um programa para calcular a media de três notas do aluno */
-// Calcular a soma das 3 notas e tirar a média do aluno//
 /* pedir para digitar as 3 notas */
+/* Ao final exibir se o aluno foi reprovado ou aprovado */
 
 int main(){
     float nota1, nota2, nota3;
     float media;
-    char aprovado[50], reprovado[50];
     
+    /* Solicitando as notas */
+
     printf("Informe sua primeira notas: \n");
     scanf("%f", &nota1);
-
     printf("Informe sua segunda notas: \n");
     scanf("%f", &nota2);
-
     printf("Informe sua terceira notas: \n");
     scanf("%f", &nota3);
 
+    /* Calculando a média */
+
     media = (nota1 + nota2 + nota3) / 3;
+
+    /* Exibir a média */
 
     printf("A média é: %.1f", media);
 
+    /* Verificar se o aluno foi aprovado*/
+
+    if (media >= 7) {
+        printf("\nAprovado");
+    } else {
+        printf("\nReprovado\n");
+    }
+    
 
     return 0;
 }
