ee2f7412bb4e5632913ab5df3a17aa333cfdcd90
rixarde89 <rixarde.4@gmail.com>
2025-08-18 21:58:30 +0000

    Comparação de cartas

--- Super Trunfo/supertrunfo.c
+++ Super Trunfo/supertrunfo.c
@@ -11,20 +11,21 @@ int main() {
     int pontosturisticos_1, pontosturisticos_2;
     float area_1, area_2, pib_1, pib_2;
     float densidadepopulacional_1, densidadepopulacional_2, pibpercapita_1, pibpercapita_2;
-    int resultado1, resultado2;
+    int resultadoA, resultadoB;
     float superpoder1, superpoder2;
-    
+    char carta1[30] = "Carta 1", carta2[30] = "carta 2";
 
+    
 //Abaixo iniciaremos a coleta de dados para a carta 1.
 
     printf("*** DESAFIO SUPER TRUNFO DE PAÍSES! ***\n\n"); 
-    printf("--- CADASTRO DA PRIMEIRA CARTA ---\n\n");
+    /*printf("--- CADASTRO DA PRIMEIRA CARTA ---\n\n");
     printf("Digite a letra do estado (apenas uma letra de A até H) :");
     scanf(" %c", &estado_1);
     printf("Digite a letra do estado com o número entre 01 a 04 para gerar o código (Ex:'A01') :");
     scanf("%s", cod_carta_1);
     printf("Digite o nome da cidade :");
-    scanf("%s", cidade_1);
+    scanf("%s", cidade_1);*/
     printf("Digite o número de habitantes :");
     scanf("%d", &populacao_1);
     printf("Digite a área territorial em quilometros quadrados :");
@@ -37,12 +38,12 @@ int main() {
 //Abaixo iniciaremos a coleta de dados para a carta 2.
     
     printf("--- CADASTRO DA SEGUNDA CARTA ---\n\n");
-    printf("Digite a letra do estado (apenas uma letra de A até H) :");
+    /*printf("Digite a letra do estado (apenas uma letra de A até H) :");
     scanf(" %c", &estado_2);
     printf("Digite a letra do estado com o número entre 01 a 04 para gerar o código (Ex:'A01') :");
     scanf("%s", cod_carta_2);
     printf("Digite o nome da cidade :");
-    scanf("%s", cidade_2);
+    scanf("%s", cidade_2);*/
     printf("Digite o número de habitantes :");
     scanf("%d", &populacao_2);
     printf("Digite a área territorial em quilometros quadrados :");
@@ -58,9 +59,9 @@ pibpercapita_1 = (float) (pib_1 * 1000000000.0f) / populacao_1;
 
 //A informações logo abaixo é do recebimentos dos dados da carta 1.
     printf("\n---CARTA 1---\n");
-    printf("Estado: %c\n", estado_1);
+    /*printf("Estado: %c\n", estado_1);
     printf("Código: %s\n", cod_carta_1);
-    printf("O nome da cidade: %s\n", cidade_1);
+    printf("O nome da cidade: %s\n", cidade_1);*/
     printf("A população da cidade: %lu\n", populacao_1);
     printf("A área da cidade: %.2f Km²\n", area_1);
     printf("O PIB da cidade: %.2f Bilhões de reais\n", pib_1);
@@ -75,25 +76,36 @@ pibpercapita_2 = (float) (pib_2 * 1000000000.0f) / populacao_2;
 
 //A informações logo abaixo é do recebimentos dos dados da carta 2.
     printf("---CARTA 2---\n");
-    printf("Estado: %c\n", estado_2);
+    /*printf("Estado: %c\n", estado_2);
     printf("Código: %s\n", cod_carta_2);
-    printf("O nome da cidade: %s\n", cidade_2);
+    printf("O nome da cidade: %s\n", cidade_2);*/
     printf("A população da cidade: %lu\n", populacao_2);
     printf("A área da cidade: %.2f Km²\n", area_2);
     printf("O PIB da cidade: %.2f Bilhões de reais\n", pib_2);
     printf("Número de pontos turísticos: %d\n", pontosturisticos_2);
     printf("Densidade populacional: %.2f hab/km²\n", densidadepopulacional_2);
-    printf("PIB per capita: %.2f Reais\n ", pibpercapita_2);
+    printf("PIB per capita: %.2f Reais\n\n ", pibpercapita_2);
 
 superpoder1 = (float) populacao_1 + area_1 + pib_1 + pontosturisticos_1 + pibpercapita_1 + (1 / densidadepopulacional_1);
 superpoder2 = (float) populacao_2 + area_2 + pib_2 + pontosturisticos_2 + pibpercapita_2 + (1 / densidadepopulacional_2);
 
+// Comparação das cartas
+    printf("--- Comparação das cartas ---\n");
+resultadoA = populacao_1 > populacao_2;
+resultadoB = populacao_2 > populacao_1;
+resultadoA = area_1 > area_2;
+resultadoB = area_2 > area_1;
+
+
 // Resultado da comparação
-
     printf("--- Resultados Do Jogo ---\n\n");
-    printf("")
-
+    printf("População: %s é %d\n", carta1, resultadoA);
+    printf("População: %s é %d\n", carta2, resultadoB);
+    printf("Área: %s é %d\n", carta1, resultadoA);
+    printf("Área: %s é %d\n", carta2, resultadoB);
     
-        return 0;
+    
+    return 0;
+
 
 }
